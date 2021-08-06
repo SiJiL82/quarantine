0074542d0a913d03b03d65e91460f3a43504de29
SiJiL82 <65114782+SiJiL82@users.noreply.github.com>
2021-08-06 13:17:42 +0100

    Game blocks (#30)
    * Add dimensions and centering for game area styling
    
    * Add brick image and reference in game code
    
    * Add brick layout config to game
    
    * Add brick layout generator to game
    
    * Add ball collision and destruction
    
    * Add brick counter to game
    
    * Add ball firing away from paddle on mouse click
    
    * Add ball sticking to the paddle until it moves
    
    * Add random launch direction for ball
    
    * Add ball launch speed variable
    
    * Rename ball and brick collision method
    
    * Add ball velocity offset when it collides with paddle
    
    * Add wall bounce extra velocity stops ball sticking
    
    * Add win condition and tweaked brick layout

--- assets/js/game.js
+++ assets/js/game.js
@@ -34,10 +34,15 @@ var game = new Phaser.Game(config);
 // Declare game variables
 var ball;
 var paddle;
+var bricks;
+var numBricks = 0;
+var ballFired = false;
+var ballLaunchSpeed = 400;
 
 function preload(){
     this.load.image('ball', 'assets/img/ball.png');
     this.load.image('paddle', 'assets/img/paddle.png');
+    this.load.image('brick-first-aid', 'assets/img/brick-first-aid.png');
 }
 
 function create(){
@@ -52,8 +57,8 @@ function create(){
     ball.body.onWorldBounds = true;
     // Lets ball bounce
     ball.setBounce(1, 1);
-    // Sets initial velocity of ball
-    ball.setVelocity(200, -200);
+    // Launch the ball on mouse click
+    this.input.on('pointerdown', releaseBall);
 
     // Create the paddle object. Applies physics, set original co-ordinates, and asigns art based on keyword as set in preloader
     paddle = this.physics.add.sprite(400, 595, 'paddle');
@@ -61,22 +66,125 @@ function create(){
     paddle.setImmovable(true)
 
     // Allows ball and paddle to collide
-    this.physics.add.collider(ball, paddle);
+    this.physics.add.collider(ball, paddle, ballPaddleCollision);
     // Listens for world boundary event, and triggers onWorldBounds
     this.physics.world.on('worldbounds', onWorldBounds);
+
+    // Create bricks
+    bricks = createBricks();
+    //Add brick and ball collision
+    this.physics.add.collider(ball, bricks, ballBrickCollsion);
 }
 
 function update(){
     // Moves the paddle along the x axis based on player input (mouse or touch)
     // Defaults to 400 (half of width declared in config) to center the paddle on load
     paddle.x = this.input.x || 400;
+    //Stick the ball to the paddle when it's not fired
+    if(!ballFired) {
+        ball.x = paddle.x;
+    }
+    //Check if player has won the round
+    checkRemainingBricks();
 }
 
 // Fires whenever a world boundary event is captured by the listener above
 // Checks if player has lost, i.e. if the ball's position on y axis is below the paddle's
 function onWorldBounds() {
+    //Fell out the bottom of the world
     if (ball.y > (paddle.y)) {
         alert('Game Over!');
+        location.reload();
+    }
+    //Collided with a wall - add velocity on collision to stop the ball getting stuck in a continuous horizontal bounce
+    else {
+        let ballXVelocity = ball.body.velocity.x;
+        let ballYVelocity = ball.body.velocity.y;
+        //If ball is moving down or perfectly horizontally when it hits a wall, add a little bit of downward velocity
+        if(ball.body.velocity.y >= 0) {
+            ballYVelocity += 0.1;
+        }
+        //If ball is moving upwards when it hits the wall, add a little bit more upwards velocity
+        else {
+            ballYVelocity -= 0.1;
+        }
+        ball.setVelocity(ballXVelocity, ballYVelocity);
+    }
+}
+
+//Brick config
+const brickConfig = {
+    width: 64,
+    height: 64
+}
+
+//Brick layout config
+const brickLayout = {
+    count: {
+        row: 2,
+        col: 12
+    },
+    offset: {
+        top: 64,
+        left: 48
+    },
+    padding: 0
+};
+
+//Create bricks on screen from config
+function createBricks() {
+    const bricks = game.scene.scenes[0].physics.add.staticGroup();
+    //Loop through the number of columns and rows in the layout definition
+    for (let column = 0; column < brickLayout.count.col; column++) {
+        for (let row = 0; row < brickLayout.count.row; row++) {
+            let brickX = (column * (brickConfig.width + brickLayout.padding)) + brickLayout.offset.left;
+            let brickY = (row * (brickConfig.height + brickLayout.padding)) + brickLayout.offset.top;
+            bricks.create(brickX, brickY, 'brick-first-aid');
+            numBricks++;
+        }
+    }
+
+    return bricks;
+}
+
+//Define what happens when a brick gets hit
+function ballBrickCollsion(ball, brick) {
+    brick.disableBody(true, true);
+    numBricks--;
+}
+
+//Set the velocity of the ball to fire up from the paddle
+function releaseBall() {
+    ball.setVelocity(getRandomBetweenRange(-200, 200), -ballLaunchSpeed);
+    //Set ball as fired so it stops sticking to the paddle
+    ballFired = true;
+}
+
+//Generate a random number between the passed in values
+function getRandomBetweenRange(min, max) {
+    if(max < min) {
+        console.log("Incorrect values passed to getRandomBetweenRange. Max should be > min");
+        return 0;
+    }
+    return Math.random() * (max - min) + min;
+}
+
+//Define what happens when the ball collides with the paddle
+function ballPaddleCollision(ball, paddle) {
+    //Get the offset between the paddle and ball
+    let offset = ball.x - paddle.x;
+    //Set a new velocity for the ball, adding velocity on X so it bounces in the direction relative to where it hit the paddle.
+    //Hitting the paddle to the left of the center applies a -X offset for example.
+    //Stops the ball getting stuck bouncing straight up and down endlessly
+    let ballXVelocity = ball.body.velocity.x += offset;
+    let ballYVelocity = ball.body.velocity.y;
+    ball.setVelocity(ballXVelocity, ballYVelocity);
+}
+
+//Check if all bricks have been destroyed
+function checkRemainingBricks() {
+    if(numBricks == 0) {
+        alert("You win!");
         location.reload();
     }
 }
